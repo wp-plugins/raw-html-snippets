0f3c523f885dbdd5555ed3210524c57875f95a6d
theandystratton <theandystratton@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-11 17:59:37 +0000

    First commit ;]
    git-svn-id: https://plugins.svn.wordpress.org/raw-html-snippets/trunk@383776 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ raw-html-snippets.php
@@ -0,0 +1,238 @@
+<?php
+/*
+Plugin Name: Raw HTML Snippets
+Plugin URI: http://theandystratton.com
+Author: theandystratton
+Author URI: http://theandystratton.com
+Description: Uses a shortcode to give users multiple methods of properly inserting RAW HTML content without disabling core WordPress content filters.
+*/
+add_shortcode('raw_html_snippet', 'rhs_raw_html_snippet_shortcode');
+function rhs_raw_html_snippet_shortcode( $atts, $content = '' ) {
+	extract( shortcode_atts(array(
+		'id' => false
+	), $atts) );
+	
+	if ( !$id ) 
+		return '';
+	
+	$snippet = get_option('rhs_snippet-' . $id);
+	
+	return $snippet;
+}
+
+add_action('admin_menu', 'rhs_raw_html_snippet_admin_menu');
+function rhs_raw_html_snippet_admin_menu() {
+	add_submenu_page('options-general.php', 'Raw HTML Snippets', 'Raw HTML Snippets', 'edit_posts', 'raw-html-snippets', 'rhs_raw_html_snippet_settings');
+}
+
+function rhs_raw_html_snippet_settings() {
+	
+	if ( $_GET['edit'] )
+		return rhs_raw_html_snippet_editor();
+		
+	if ( $_GET['add'] )
+		return rhs_raw_html_snippet_add();
+	
+	$errors = array();
+	$clean = array();
+	
+	if ( $_GET['rhs_del'] && wp_verify_nonce($_GET['rhs_nonce'], 'rhs_delete') ) {
+		delete_option('rhs_snippet-' . $_GET['rhs_del']);
+		$snippet_list = get_option('rhs_snippet_list');
+		if ( is_array($snippet_list) && in_array($_GET['rhs_del'], $snippet_list) ) {
+			$snippet_list = array_diff( $snippet_list, array( $_GET['rhs_del'] ) );
+			update_option('rhs_snippet_list', $snippet_list);
+			$success = 'Snippet with ID &quot;' . esc_html($_GET['rhs_del']) . '&quot; successfully deleted.';
+		}
+	}
+
+	
+	$snippet_list = get_option('rhs_snippet_list');
+	if ( !is_array($snippet_list) )
+		$snippet_list = array();
+	
+?>
+<div class="wrap">
+	<h2>Manage Raw HTML Snippets</h2>
+	<p>
+		Create and manage your HTML snippets here. Name them with an id (letters, numbers, and dashes only) and then
+		call them within your content via shortcode: <br />
+		<code>[raw_html_snippet id="my-unique-id"]</code>
+	</p>
+	<p>
+		This will use maintain core WordPress content filtering and autoformatting for all other elements within your content while 
+		allowing you to easily insert managed RAW HTML.
+	</p>
+	<p>
+		<strong>WARNGING:</strong> This does not filter your HTML for errors or
+		for malicious scripts. Use at your own risk.
+	</p>
+	
+	<?php if ( count($snippet_list) > 0 ) : ?>
+	
+	
+	<form method="get" action="">
+		<p class="alignright">
+			<input type="hidden" name="page" value="raw-html-snippets" />
+			<input type="hidden" name="add" value="1" />
+			<input type="submit" class="button-primary" value="Add a New Raw HTML Snippet &raquo;" />
+		</p>
+	</form>
+	
+	<h2>Your Snippet Library</h2>
+	
+	<table class="widefat fixed">
+	<thead>
+	<tr>
+		<th>Snippet Name</th>
+		<th>Shortcode</th>
+		<th>Actions</th>
+	</tr>
+	</thead>
+	<tbody>
+	
+	<?php foreach ( $snippet_list as $snippet_id ) : ?>
+	<tr>
+		<td>
+			<?php echo esc_html($snippet_id);?>
+			<div class="row-actions">
+				<a href="?page=raw-html-snippets&amp;edit=<?php echo esc_attr($snippet_id); ?>">Edit</a> | 
+				<span class="trash"><a class="submitdelete" onclick="return confirm('Are you sure you want to delete this snippet?');" href="?page=raw-html-snippets&amp;rhs_nonce=<?php echo esc_attr(wp_create_nonce('rhs_delete')); ?>&amp;rhs_del=<?php echo esc_attr($snippet_id); ?>">Delete</a></span>
+			</div>
+		</td>
+		<td>
+			<code>[raw_html_snippet id="<?php echo esc_html($snippet_id); ?>"]</code>
+		</td>
+		<td>
+			<a href="?page=raw-html-snippets&amp;edit=<?php echo esc_attr($snippet_id); ?>">Edit Snippet</a> | 
+			<span class="trash"><a onclick="return confirm('Are you sure you want to delete this snippet?');" href="?page=raw-html-snippets&amp;rhs_nonce=<?php echo esc_attr(wp_create_nonce('rhs_delete')); ?>&amp;rhs_del=<?php echo esc_attr($snippet_id); ?>">Delete Snippet</a></span>
+		</td>
+	</tr>
+	<?php endforeach; ?>
+	
+	</tbody>
+	</table>
+	
+	<?php else : ?>
+		<h2>Your Snippets Library is Empty</h2>
+		<p>You have no snippets, please <a href="?page=raw-html-snippets&amp;add=1">please add one</a>.</p>
+	<?php endif; ?>
+</div>
+<?php 
+}
+
+function rhs_raw_html_snippet_editor() {
+	$snippet_id = $_GET['edit'];
+	$errors = array();
+	if ( !empty($_POST) && wp_verify_nonce($_POST['rhs_nonce'], 'rhs_nonce') ) {
+		$snippet = stripslashes($_POST['snippet_code']);
+		if ( empty($snippet) ) 
+			$errors[] = 'Enter some HTML for this snippet.';
+		if ( count($errors) <= 0 ) {
+			update_option('rhs_snippet-' . $snippet_id, $snippet);
+			$success = 'Your changes have been saved.';
+		}
+	}
+	$snippet = get_option('rhs_snippet-' . $snippet_id);
+	$clean = array(
+		'snippet_code' => $snippet
+	);
+?>
+<div class="wrap">
+	<h2>Edit Raw HTML Snippet: &quot;<?php echo esc_html($snippet_id); ?>&quot;</h2>
+	<p><a href="?page=raw-html-snippets">&laquo; Back to main page</a></p>
+	<form method="post" action="">
+		
+		<?php if ( count($errors) > 0 ) : ?>
+		<div class="message error"><?php echo wpautop(implode("\n", $errors)); ?></div>
+		<?php endif; ?>
+		<?php if ( $success ) : ?>
+		<div class="message updated"><?php echo wpautop($success); ?></div>
+		<?php endif; ?>
+		
+		<?php wp_nonce_field('rhs_nonce', 'rhs_delete'); ?>
+		
+		<p><label for="snippet_code">Snippet Code:</label></p>
+		<textarea id="snippet_code" name="snippet_code" rows="10" style="font-family:Monaco,'Courier New',Courier,monospace;font-size:12px;width:80%;color:#555;"><?php
+			if ( isset($clean['snippet_code']) )
+				echo esc_attr($clean['snippet_code']);
+		?></textarea>
+		
+		<p>
+			<input type="submit" class="button-primary" value="Save Snippet &raquo;" /> 
+			<input type="button" class="button" value="Delete This Snippet" onclick="if ( confirm('Are you sure you want to delete this snippet?') ) window.location = '?page=raw-html-snippets&amp;rhs_del=<?php echo esc_attr($snippet_id); ?>&amp;rhs_nonce=<?php echo esc_attr(wp_create_nonce('rhs_delete')); ?>';" />
+		</p>
+	</form>	
+</div>
+<?php
+}
+
+function rhs_raw_html_snippet_add() {
+	$snippet_list = get_option('rhs_snippet_list');
+	if ( !is_array($snippet_list) )
+		$snippet_list = array();
+		
+	$errors = array();
+	$clean = array();
+	
+	if ( !empty($_POST) && wp_verify_nonce($_POST['rhs_nonce'], 'rhs_nonce') ) {
+
+		foreach ( $_POST as $k => $v )
+			$clean[$k] = stripslashes($v); 
+			
+		if ( empty($clean['snippet_id']) ) 
+			$errors[] = 'Please enter a unique snippet ID.';
+		elseif ( in_array(strtolower($clean['snippet_id']), $snippet_list) )
+			$errors[] = 'You have entered a snippet ID that already exists. IDs are NOT case-sensitive.';
+		
+		if ( empty($clean['snippet_code']) ) 
+			$errors[] = 'Enter some HTML for this snippet.';
+		
+		if ( count($errors) <= 0 ) {
+			// save snippet
+			$snippet_id = strtolower($clean['snippet_id']);
+			$snippet_list[] = $snippet_id;
+			update_option('rhs_snippet_list', $snippet_list);
+			update_option('rhs_snippet-' . $snippet_id, $clean['snippet_code']);
+			$success = 'Your snippet has been saved.';
+			$clean = array();
+		}
+	}
+	
+?>
+<div class="wrap">
+	<h2>Add Raw HTML Snippet:</h2>
+	
+	<p><a href="?page=raw-html-snippets">&laquo; Back to main page</a></p>
+		
+	<form method="post" action="" style="margin: 1em 0;padding: 1px 1em;background: #fff;border: 1px solid #ccc;">
+		
+		<?php if ( count($errors) > 0 ) : ?>
+		<div class="message error"><?php echo wpautop(implode("\n", $errors)); ?></div>
+		<?php endif; ?>
+		<?php if ( $success ) : ?>
+		<div class="message updated"><?php echo wpautop($success); ?></div>
+		<?php endif; ?>
+		
+		<?php wp_nonce_field('rhs_nonce', 'rhs_nonce'); ?>
+		
+		<p>
+			<label for="snippet_id">Snippet ID:</label>
+			<br />
+			<input type="text" name="snippet_id" id="snippet_id" size="40" value="<?php
+			if ( isset($clean['snippet_id']) ) 
+				echo esc_attr($clean['snippet_id']);
+		?>" />
+		</p>
+		
+		<p><label for="snippet_code">Snippet Code:</label></p>
+		<textarea id="snippet_code" name="snippet_code" rows="10" style="font-family:Monaco,'Courier New',Courier,monospace;font-size:12px;width:80%;color:#555;"><?php
+			if ( isset($clean['snippet_code']) )
+				echo esc_attr($clean['snippet_code']);
+		?></textarea>
+		
+		<p><input type="submit" class="button-primary" value="Add Snippet &raquo;" />
+	</form>	
+</div>
+<?php	
+}
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Raw HTML Snippet ===
+Contributors: theandystratton
+Donate link: http://theandystratton.com
+Tags: raw html, html, embed html, autoformatting
+Requires at least: 2.6
+Tested up to: 3.1.2
+Stable tag: trunk
+
+== Description ==
+
+Create a library of raw HTML snippets that you can easily insert into any page/post content using a shortcode:
+
+[raw_html_snippet id="my-snippet"]
+
+Snippets consist of a unique ID (e.g. "my-snippet") and raw HTML code. This plugin was written to stop using hacks that override WordPress' core content filters and affect shortcode output.
+
+This plugin will NOT taint your content or the output of other shortcodes. If you delete a snippet, any existing shortcodes with that snipet's ID will output an empty string.
+
+== Installation ==
+
+1. Download and unzip to the 'wp-content/plugins/' directory 
+2. Activate the plugin.
+3. Use Settings > Raw HTML Snippets to add, edit and remove your snippets.
+4. Insert snippets at your will
+
+== Screenshots ==
+
+1. Plugin admin screen.
+2. Editing a snippet
+3. Inserting a snippet
